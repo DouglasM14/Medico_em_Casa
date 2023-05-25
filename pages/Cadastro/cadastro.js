2a8d2bc447ae44ac86c46cbbc387eb28f9a0f889
Douglas <82346006+DouglasM14@users.noreply.github.com>
2023-05-25 16:07:33 -0300

    1.5
    Código principal da pagina de cadastro completo

---
+++ pages/Cadastro/cadastro.js
@@ -0,0 +1,27 @@
+function continuar1(){
+    document.getElementById("bloco1").classList.add('esconder')
+    document.getElementById("bloco2").classList.remove('esconder')
+}
+function continuar2(){
+    document.getElementById("bloco2").classList.add('esconder')
+    document.getElementById("bloco3").classList.remove('esconder')
+}
+function continuar3(){
+    document.getElementById("bloco3").classList.add('esconder')
+    document.getElementById("bloco4").classList.remove('esconder')
+}
+function Finalizar(){
+    console.log("formulario enviado")
+}
+function voltar2(){
+    document.getElementById("bloco2").classList.add('esconder')
+    document.getElementById("bloco1").classList.remove('esconder')
+}
+function voltar3(){
+    document.getElementById("bloco3").classList.add('esconder')
+    document.getElementById("bloco2").classList.remove('esconder')
+}
+function voltar4(){
+    document.getElementById("bloco4").classList.add('esconder')
+    document.getElementById("bloco3").classList.remove('esconder')
+}
