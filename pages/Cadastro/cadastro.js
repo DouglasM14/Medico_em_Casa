937c39bd4579a81600508808bc9e3bf9a7c7981c
Douglas <82346006+DouglasM14@users.noreply.github.com>
2023-05-25 17:15:24 -0300

    1.5.1

--- pages/Cadastro/cadastro.js
+++ pages/Cadastro/cadastro.js
@@ -1,27 +1,27 @@
-function continuar1(){
+function continuar1() {
     document.getElementById("bloco1").classList.add('esconder')
     document.getElementById("bloco2").classList.remove('esconder')
 }
-function continuar2(){
+function continuar2() {
     document.getElementById("bloco2").classList.add('esconder')
     document.getElementById("bloco3").classList.remove('esconder')
 }
-function continuar3(){
+function continuar3() {
     document.getElementById("bloco3").classList.add('esconder')
     document.getElementById("bloco4").classList.remove('esconder')
 }
-function Finalizar(){
+function Finalizar() {
     console.log("formulario enviado")
 }
-function voltar2(){
+function voltar2() {
     document.getElementById("bloco2").classList.add('esconder')
     document.getElementById("bloco1").classList.remove('esconder')
 }
-function voltar3(){
+function voltar3() {
     document.getElementById("bloco3").classList.add('esconder')
     document.getElementById("bloco2").classList.remove('esconder')
 }
-function voltar4(){
+function voltar4() {
     document.getElementById("bloco4").classList.add('esconder')
     document.getElementById("bloco3").classList.remove('esconder')
 }
