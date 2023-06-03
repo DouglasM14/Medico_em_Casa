a2506abb04efa1805db0174cca2f91e1cd34d47a
Douglas <82346006+DouglasM14@users.noreply.github.com>
2023-06-03 16:36:43 -0300

    1.13
    Validações da pag cadastro quase completas

--- pages/Pag_Cadastro/cadastro.js
+++ pages/Pag_Cadastro/cadastro.js
@@ -17,79 +17,23 @@ btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
 
-// const form = document.getElementById('form');
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-// const cep = document.getElementById('cep');
-// const rua = document.getElementById('rua');
-// const numeroCasa = document.getElementById('numeroCasa');
-// const complemento = document.getElementById('complemento');
-// const email = document.getElementById('email');
-// const telefone = document.getElementById('telefone');
-// const senha = document.getElementById('senha');
-// const senha2 = document.getElementById('senha2');
-// const doenças = document.getElementById('doenças');
-// const alergias = document.getElementById('alergias');
-// const condiçõesGeneticas = document.getElementById('condiçõesGeneticas');
-// const condiçõesPsiquiátricas = document.getElementById('condiçõesPsiquiátricas');
-// const cirurgias = document.getElementById('cirurgias');
-// const historico = document.getElementById('historico');
-
-// console.log(form);
-// console.log(nome);
-// console.log(cpf);
-// console.log(nome);
-// console.log(genero);
-// console.log(cep);
-// console.log(rua);
-// console.log(numeroCasa);
-// console.log(complemento);
-// console.log(email);
-// console.log(telefone);
-// console.log(senha);
-// console.log(senha2);
-// console.log(doenças);
-// console.log(alergias);
-// console.log(condiçõesGeneticas);
-// console.log(condiçõesPsiquiátricas);
-// console.log(cirurgias);
-// console.log(historico);
-
-// function continuar1() {
-//     const nome = document.getElementById('nome');
-//     const cpf = document.getElementById('cpf');
-//     const rg = document.getElementById('rg');
-//     const genero = document.getElementById('genero');
-//     validarInputs()
-// }
+console.log(campos)
 
 
-// function validarInputs() {
+function setError(index) {
+    campos[index].style.border = '2px solid red'
+    spans[index].style.display = 'block'
+}
 
-//     if (nome === '') {
-//         erro(nome, 'Nome invalido');
-//     } else {
-//         sucesso(nome);
-//     }
-
-// }
-
-// const erro = (element, message) => {
-//     const inputControl = element.parentElement;
-//     const errorDisplay = inputControl.querySelector('.error');
-
-//     errorDisplay.innerText = message;
-//     inputControl.classList.add('error');
-//     inputControl.classList.remove('success')
-// }
-
-// const setSuccess = element => {
-//     const inputControl = element.parentElement;
-//     const errorDisplay = inputControl.querySelector('.error');
-
-//     errorDisplay.innerText = '';
-//     inputControl.classList.add('success');
-//     inputControl.classList.remove('error');
-// };
+function removeError(index) {
+    campos[index].style.border = ''
+    spans[index].style.display = 'none'
+}
 
 
 
@@ -97,33 +41,223 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 
 
 
-// https://codepen.io/javascriptacademy-stash/pen/oNeNMNR?editors=0010
 
 
-// function continuar1() {
-//     document.getElementById("bloco1").classList.add('esconder')
-//     document.getElementById("bloco2").classList.remove('esconder')
-// }
-// function continuar2() {
-//     document.getElementById("bloco2").classList.add('esconder')
-//     document.getElementById("bloco3").classList.remove('esconder')
-// }
-// function continuar3() {
-//     document.getElementById("bloco3").classList.add('esconder')
-//     document.getElementById("bloco4").classList.remove('esconder')
-// }
-// function Finalizar() {
-//     console.log("formulario enviado")
-// }
-// function voltar2() {
-//     document.getElementById("bloco2").classList.add('esconder')
-//     document.getElementById("bloco1").classList.remove('esconder')
-// }
-// function voltar3() {
-//     document.getElementById("bloco3").classList.add('esconder')
-//     document.getElementById("bloco2").classList.remove('esconder')
-// }
-// function voltar4() {
-//     document.getElementById("bloco4").classList.add('esconder')
-//     document.getElementById("bloco3").classList.remove('esconder')
-// }
+//                           BLOCO 1
+function nameValidate() {
+    if (campos[0].value === '') {
+        setError(0)
+    } else {
+        removeError(0)
+    }
+}
+
+function dateValidate() {
+    if (campos[1].value === '') {
+        setError(1)
+    } else {
+        removeError(1)
+    }
+}
+
+function cpfValidate() {
+    if (campos[2].value.length < 11) {
+        setError(2)
+    } else {
+        removeError(2)
+    }
+}
+
+function rgValidate() {
+    if (campos[3].value.length < 9) {
+        setError(3)
+    } else {
+        removeError(3)
+    }
+}
+
+function continuar1() {
+    let numValidar = 0;
+
+    if (campos[0].value === '') {
+        setError(0)
+    } else {
+        removeError(0)
+        numValidar++
+    }
+    if (campos[1].value === '') {
+        setError(1)
+    } else {
+        removeError(1)
+        numValidar++
+    }
+    if (campos[2].value.length < 11) {
+        setError(2)
+    } else {
+        removeError(2)
+        numValidar++
+    }
+    if (campos[3].value.length < 9) {
+        setError(3)
+    } else {
+        removeError(3)
+        numValidar++
+    }
+    if (numValidar == 4) {
+        document.getElementById("bloco1").classList.add('esconder')
+        document.getElementById("bloco2").classList.remove('esconder')
+    }
+}
+
+
+
+
+
+
+
+
+
+//                           BLOCO 2
+function ruaValidate() {
+    if (campos[4].value === '') {
+        setError(4)
+    } else {
+        removeError(4)
+    }
+}
+
+function numRuaValidate() {
+    if (campos[5].value === '') {
+        setError(5)
+    } else {
+        removeError(5)
+    }
+}
+
+function cepValidate() {
+    if (campos[6].value.length < 11) {
+        setError(6)
+    } else {
+        removeError(6)
+    }
+}
+
+
+function continuar2() {
+    let numValidar = 0;
+
+    if (campos[4].value === '') {
+        setError(4)
+    } else {
+        removeError(4)
+        numValidar++
+    }
+    if (campos[5].value === '') {
+        setError(5)
+    } else {
+        removeError(5)
+        numValidar++
+    }
+    if (campos[6].value === '') {
+        setError(6)
+    } else {
+        removeError(6)
+        numValidar++
+    }
+
+    if (numValidar == 3) {
+        document.getElementById("bloco2").classList.add('esconder')
+        document.getElementById("bloco3").classList.remove('esconder')
+    }
+}
+function voltar2() {
+    document.getElementById("bloco2").classList.add('esconder')
+    document.getElementById("bloco1").classList.remove('esconder')
+}
+
+
+
+
+
+
+
+
+
+//                           BLOCO 3
+function emailValidate() {
+    if (emailRegex.test(campos[7].value)) {
+        removeError(7)
+    } else {
+        setError(7)
+    }
+}
+
+function telValidate() {
+    if (campos[8].value.length < 11) {
+        setError(8)
+    } else {
+        removeError(8)
+    }
+}
+
+function senhaValidate() {
+    if (campos[9].value.length < 8) {
+        setError(9)
+    } else {
+        removeError(9)
+    }
+}
+
+function compararSenha() {
+    if (campos[9].value == campos[10].value && campos[10].value.length >= 8) {
+        removeError(10)
+    } else {
+        setError(10)
+    }
+}
+
+function continuar3() {
+    let numValidar = 0;
+
+    if (emailRegex.test(campos[7].value)) {
+        removeError(7)
+        numValidar++
+    } else {
+        setError(7)
+    }
+    if (campos[8].value.length < 11) {
+        setError(8)
+    } else {
+        removeError(8)
+        numValidar++
+    }
+    if (campos[9].value.length < 8) {
+        setError(9)
+    } else {
+        removeError(9)
+        numValidar++
+    }
+    if (campos[9].value == campos[10].value && campos[10].value.length >= 8) {
+        removeError(10)
+        numValidar++
+    } else {
+        setError(10)
+    }
+    if (numValidar == 4) {
+        document.getElementById("bloco3").classList.add('esconder')
+        document.getElementById("bloco4").classList.remove('esconder')
+    }
+}
+function voltar3() {
+    document.getElementById("bloco3").classList.add('esconder')
+    document.getElementById("bloco2").classList.remove('esconder')
+}
+
+function voltar4() {
+    document.getElementById("bloco4").classList.add('esconder')
+    document.getElementById("bloco3").classList.remove('esconder')
+}
+
+function Finalizar() {
+    console.log("formulario enviado")
+}
