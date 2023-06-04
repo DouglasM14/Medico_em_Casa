8ca911025107d636691d4ae1f775c000f8ec9b56
Douglas <82346006+DouglasM14@users.noreply.github.com>
2023-06-03 21:36:49 -0300

    1.14
    Adição de algumas paginas e validações de dados

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -15,3 +15,29 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+
+function setError(index) {
+  campos[index].style.border = '2px solid red'
+  spans[index].style.display = 'block'
+}
+
+function removeError(index) {
+  campos[index].style.border = ''
+  spans[index].style.display = 'none'
+}
+
+function verificarLogin(){
+  if (campos[0].value === '') {
+    setError(0)
+} else {
+    removeError(0)
+}
+if (campos[1].value === '') {
+    setError(1)
+} else {
+    removeError(1)
+}
+}
