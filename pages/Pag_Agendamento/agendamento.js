fdbb1d1e10d59c51992784b080b28bc19b11f1c7
Douglas <82346006+DouglasM14@users.noreply.github.com>
2023-06-05 22:55:43 -0300

    1.15.1

--- pages/Pag_Agendamento/agendamento.js
+++ pages/Pag_Agendamento/agendamento.js
@@ -15,3 +15,25 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+function validarData() {
+  // Obter o valor do input
+  var inputDate = document.getElementById('date-input').value;
+
+  // Obter a data atual
+  var dataAtual = new Date();
+
+  // Converter a data selecionada e a data atual em objetos Date
+  var selectedDate = new Date(inputDate);
+  var currentDate = new Date(dataAtual.getFullYear(), dataAtual.getMonth(), dataAtual.getDate());
+
+  // Comparar a data selecionada com a data atual
+  if (selectedDate < currentDate) {
+    // Definir a data atual como valor do input
+    var dia = ('0' + dataAtual.getDate()).slice(-2);
+    var mes = ('0' + (dataAtual.getMonth() + 1)).slice(-2);
+    var ano = dataAtual.getFullYear();
+    var dataAtualFormatted = ano + '-' + mes + '-' + dia;
+    document.getElementById('date-input').value = dataAtualFormatted;
+  }
+}
