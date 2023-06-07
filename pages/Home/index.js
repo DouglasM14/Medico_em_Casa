c709099ab0588b366dd43e43afd2a1b5ba322af5
Douglas <82346006+DouglasM14@users.noreply.github.com>
2023-06-06 21:08:06 -0300

    2.0
    site caminhando pra reta final

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -29,15 +29,18 @@ function removeError(index) {
   spans[index].style.display = 'none'
 }
 
-function verificarLogin(){
+function verificarLogin() {
   if (campos[0].value === '') {
     setError(0)
-} else {
+  } else {
     removeError(0)
-}
-if (campos[1].value === '') {
+  }
+  if (campos[1].value === '') {
     setError(1)
-} else {
+  } else {
     removeError(1)
-}
+  }
+  if (campos[0].value == 'jesusmedico@gmail.com' && campos[1].value == 'jesus123') {
+    document.getElementById('popup').classList.remove('esconder')
+  }
 }
